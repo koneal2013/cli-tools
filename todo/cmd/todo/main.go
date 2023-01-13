04258e8249530b3af092e1e4f6ad81e659c9d0bd
Kenston O'Neal <koneal2013@gmail.com>
2023-01-13 18:46:06 -0500

    small bug fixes

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -18,6 +18,7 @@ func main() {
 	complete := flag.Int("complete", 0, "Item to be completed")
 	del := flag.Int("del", 0, "Item to be deleted")
 	verbose := flag.Bool("verbose", false, "Enable verbose output, showing information like date/time")
+	removeCompleted := flag.Bool("remove_completed", false, "Remove completed tasks when the 'list' command is executed")
 	flag.Parse()
 
 	var todoFileName = ".todo.json"
@@ -31,9 +32,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	withoutCompleted := &todo.List{}
 	switch {
 	case *list:
+		if *removeCompleted {
+			for _, t := range *l {
+				if !t.Done {
+					*withoutCompleted = append(*withoutCompleted, t)
+				}
+			}
+			if !*verbose {
+				fmt.Print(withoutCompleted)
+				os.Exit(0)
+			}
+		}
 		if *verbose {
+			if *removeCompleted {
+				fmt.Print(withoutCompleted.StringVerbose())
+				os.Exit(0)
+			}
 			fmt.Print(l.StringVerbose())
 			os.Exit(0)
 		}
